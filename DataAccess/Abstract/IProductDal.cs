b401b865976c372932f2c0f3b95e945053220dd8
Doğukan ASLAN <dogukanaslan_@hotmail.com>
2022-02-27 17:11:24 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Productla ilgili veri tabanımda yapacağım operasyonlar
+    public interface IProductDal : IEntityRepository<Product>
+    {
+
+    }
+}
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity implement eden bir class veritabanı tablosudur.
+    public interface IEntity
+    {
+    }
+}
+//IEntity demek bu veri tabanındaki bir nesne demek.
