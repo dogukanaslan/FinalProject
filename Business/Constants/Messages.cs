1abafc51d7b3794278f468884217c1034f0697c1
Doğukan ASLAN <dogukanaslan_@hotmail.com>
2022-03-04 17:53:45 +0300

    <

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi.";
+        public static string ProductNameInValid = "Ürün İsmi Geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductsListed = "Ürünler listelendi";
+    }
+}
---
+++ Core1/Utilities/Results/IResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core1.Utilities.Results
+{
+    //Temel voidler için başlangıç
+    public interface IResult
+    {
+        bool Success { get; }
+        string Message { get; }
+    }
+}
+
+//Sadece get olduğu için productmanagerda metotta return kullanmamız lazım.
+//Ama Getterlar(get) read only dir ve constructor içinde set edilebilirler.
+//Set etmememizin nedeni yazılımcıyı kısıtlamak ve kodları standart hale getirmektir bundan dolatı constructor içinde set yaparız.
